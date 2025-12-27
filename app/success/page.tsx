eb45bd117dfc4ad1f70f9dc76ed733588c57cc06
Ali Haider <ah0062527@gmail.com>
2025-12-27 16:23:14 +0500

    register api completed

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,11 +1,12 @@
+
 export default function SuccessPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+    <main className="min-h-screen flex items-center justify-center p-4" style={{ backgroundColor: '#3a4095' }}>
       <div className="bg-white p-6 rounded-xl shadow text-center space-y-2">
-        <h1 className="text-2xl font-semibold">Registration Successful 🎉</h1>
+        <h1 className="text-2xl font-semibold text-gray-600">Registration Successful 🎉</h1>
         <p className="text-sm text-gray-600">
           Your registration has been received.  
-          You’ll get a confirmation email soon.
+          You&apos;ll get a confirmation email soon.
         </p>
       </div>
     </main>
